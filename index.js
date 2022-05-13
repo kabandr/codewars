79d64552d5c1f3681d56c23598a47c9c09039e06
Rene Kabanda <2495191+kabandr@users.noreply.github.com>
2022-05-13 17:09:30 +0100

    Auto-commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import shell from "shelljs";
 // Run external tool synchronously
 if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
     shell.exec('git add .');
-    shell.exec('git add commit -m .');
+    shell.exec('git add commit -m ');
   shell.echo("Error: Git commit failed");
   shell.exit(1);
 }
