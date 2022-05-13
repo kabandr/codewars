75f0d718ea348082cd822d88bb83d1df61d78ac7
Rene Kabanda <2495191+kabandr@users.noreply.github.com>
2022-05-13 20:31:50 +0100

    Auto-commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import shell from "shelljs";
 // Run external tool synchronously
 if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
   shell.exec("git add .");
-  shell.exec('git commit -m "fixed"');
+  shell.exec('git commit -m "fixed build errors"');
   shell.exec("git push origin master");
   shell.echo("Successfully pushed to github repo!");
   shell.exit(1);
