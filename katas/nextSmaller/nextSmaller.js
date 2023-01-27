94305ea5d6aec4af49158e44608e75e13288c8da
Rene Kabanda <renerugwiro@yahoo.fr>
2023-01-27 13:38:01 +0000

    Solution for Next smaller number with the same digits

--- katas/nextSmaller/nextSmaller.js
+++ katas/nextSmaller/nextSmaller.js
@@ -1,6 +1,29 @@
-export const nextSmaller = (n) => {
-    const str = n.toString();
-    return str
+function nextSmaller(n) {
+    let digits = n.toString().split('');
+    let newIndex = 0;
+    let temp;
+    let found = false;
+
+    for (let i = 0; i < digits.length - 1; i++) {
+        if (digits[i] > digits[i + 1]) {
+            newIndex = i;
+            found = true;
+        }
+    }
+    if (!found) return -1;
+    
+    for (let i = digits.length - 1; i > 0; i--) {
+        if (i == newIndex) i--;
+        if (digits[i] < digits[newIndex]) {
+            temp = digits[i];
+            digits[i] = digits[newIndex];
+            digits[newIndex] = temp;
+            break;
+        }
+    }
+
+    if (digits[0] == 0) return -1
+    return parseInt(digits.slice(0, newIndex + 1).concat(digits.slice(newIndex + 1).sort((a, b) => b - a)).join(''))
 }
 
-console.log(nextSmaller(21))
+module.exports = nextSmaller
--- katas/nextSmaller/nextSmaller.test.js
+++ katas/nextSmaller/nextSmaller.test.js
@@ -1,19 +1,45 @@
 const nextSmaller = require("./nextSmaller")
 
-describe("Fixed tests", function() {
-	it("Smaller numbers", function() {
-        expect(nextSmaller(21)).toBe(12);
-        expect(nextSmaller(907)).toBe(790)
-		expect(nextSmaller(531)).toBe(513)
-		expect(nextSmaller(135)).toBe(-1)
-		expect(nextSmaller(2071)).toBe(2017)
-		expect(nextSmaller(1027)).toBe(-1)
-		expect(nextSmaller(414)).toBe(144)
-	})
-  
-	it("Bigger numbers", function() {
-		expect(nextSmaller(123456798)).toBe(123456789)
-		expect(nextSmaller(123456789)).toBe(-1)
-		expect(nextSmaller(1234567908)).toBe(1234567890)
-	})
-})
+test('nextSmaller test case 1', () => {
+    expect(nextSmaller(21)).toBe(12);
+});
+
+test('nextSmaller test case 2', () => {
+    expect(nextSmaller(907)).toBe(790);
+});
+
+test('nextSmaller test case 3', () => {
+    expect(nextSmaller(531)).toBe(513);
+});
+
+test('nextSmaller test case 4', () => {
+    expect(nextSmaller(135)).toBe(-1);
+});
+
+test('nextSmaller test case 5', () => {
+    expect(nextSmaller(2071)).toBe(2017);
+});
+
+test('nextSmaller test case 6', () => {
+    expect(nextSmaller(414)).toBe(144);
+});
+
+test('nextSmaller test case 7', () => {
+    expect(nextSmaller(135)).toBe(-1);
+});
+
+test('nextSmaller test case 8', () => {
+    expect(nextSmaller(1027)).toBe(-1);
+});
+
+test('nextSmaller test case 9', () => {
+    expect(nextSmaller(123456798)).toBe(123456789);
+});
+
+test('nextSmaller test case 10', () => {
+    expect(nextSmaller(123456789)).toBe(-1);
+});
+
+test('nextSmaller test case 11', () => {
+    expect(nextSmaller(1234567908)).toBe(1234567890);
+});
